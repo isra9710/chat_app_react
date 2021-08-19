f00ad76d746bdb44b09902a982bf3c8a574899be
isra9710 <israel.rios.contreras@getwoobie.com>
2021-08-19 13:21:35 -0500

    1.0

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import {ChatEngine} from "react-chat-engine"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
